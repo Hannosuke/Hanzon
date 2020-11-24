dda0b5831c7e97c327fa3f1f32ad253d5bd9ab13
HandaR <midrau18@gamil.com>
2020-11-24 13:09:23 +0900

    ログイン機能を一通り記述しました

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -29,6 +29,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
+  def verify
+  end
+
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
@@ -59,4 +62,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def after_inactive_sign_up_path_for(resource)
+    verify_path
+  end
 end
