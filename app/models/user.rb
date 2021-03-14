b3b67578b7fa57141a424928b0af95a180f3c133
HandaR <midrau18@gamil.com>
2021-03-14 14:35:05 +0900

    25 create_user_management

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,21 @@
 class User < ApplicationRecord
   has_many :reviews
+  extend DisplayList
+  extend SwitchFlg
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
   acts_as_liker
+
+  scope :search_information, -> (keyword) { 
+      where("name like ?","%#{keyword}%").
+      or(where("email like ?","%#{keyword}%")).
+      or(where("address like ?","%#{keyword}%")).
+      or(where("postal_code like ?","%#{keyword}%")).
+      or(where("phone like ?","%#{keyword}%")).
+      or(where("id ?","%#{keyword}%"))
+    }
 
   def update_password(params, *optionss)
     if params[:password].blank?
